3c76d5a9a8a08f227ac7f7c6ee2e3df624fd3a83
Bailey-Kerr-Mav <107577926+Bailey-Kerr-Mav@users.noreply.github.com>
2024-04-03 16:13:31 -0400

    Update mavSegmenationScript2.js

--- mavSegmenationScript2.js
+++ mavSegmenationScript2.js
@@ -1,7 +1,7 @@
 (function() {
     const element = window.document.createElement('script');
     const user_name = "#{securityContext.userName}";
-    const Employee = #{securityContext.userInRole['ORA_PER_EMPLOYEE_ABSTRACT']},
+    const Employee = "#{securityContext.userInRole['ORA_PER_EMPLOYEE_ABSTRACT']}";
     element.setAttribute('src', user_name + Employee);
     window.document.head.appendChild(element);
 })();
